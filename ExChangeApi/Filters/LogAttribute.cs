48ed44ea70577652f12c4e9bfd2c6b8faaaa8c34
Farbod-developer501 <mr.farbodmirzaee@gmail.com>
2024-03-11 12:41:46 +0330

    Fix Filters in Log attribute

---
+++ ExChangeApi/Filters/LogAttribute.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace ExchangeApi.Filter;
+
+public class LogAttribute : Attribute , IActionFilter
+{
+
+    public LogAttribute() 
+    {
+    
+    }
+
+    public void OnActionExecuted(ActionExecutedContext context)
+    {
+        Console.WriteLine(string.Format("Action Method {0} Exequting at {1}", context.ActionDescriptor));
+    }
+
+    public void OnActionExecuting(ActionExecutingContext context)
+    {
+        Console.WriteLine(string.Format("Action Method {0} Exequted at {1}", context.ActionDescriptor));
+    }
+    public bool AllowMultiple
+    {
+        get { return true; }
+    }
+}
