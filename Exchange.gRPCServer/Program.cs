e2dc9b6de8151ef087320683eb02755993f7492d
Farbod <mr.farbodmirzaee@gmail.com>
2024-06-14 22:18:53 +0330

    Fix Some Issue

--- Exchange.gRPCServer/Program.cs
+++ Exchange.gRPCServer/Program.cs
@@ -19,5 +19,5 @@ app.MapGrpcReflectionService();
 app.MapGrpcService<GreeterService>();
 app.MapGrpcService<gRPCCurrencyService>();
 app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
-
+ 
 app.Run();
