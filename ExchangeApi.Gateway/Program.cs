f0b66f07760655ca4b8f27e36319c69d5a66fe27
Farbod <mr.farbodmirzaee@gmail.com>
2024-07-09 20:38:34 +0330

    Delete Useless project

--- ExchangeApi.Gateway/Program.cs
+++ ExchangeApi.Gateway/Program.cs
@@ -1,10 +1,14 @@
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
+builder.Configuration.AddJsonFile("Oselot.json", optional: false, reloadOnChange: true);
+builder.Services.AddOcelot(builder.Configuration);
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
@@ -18,8 +22,6 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-app.UseAuthorization();
-
-app.MapControllers();
+app.UseOcelot();
 
 app.Run();
