41412e1c8a59a9f2b18c8c339e38a9563add47f0
Farbod <mr.farbodmirzaee@gmai.com>
2024-11-07 10:42:34 +0330

    Add download file for grpc style

---
+++ Exchange.gRPCServer/Services/DownloadFileService.cs
@@ -0,0 +1,45 @@
+﻿using Exchange.gRPCServer.Context;
+using Exchange.gRPCServer.Protos;
+using Grpc.Core;
+using Microsoft.EntityFrameworkCore;
+
+namespace Exchange.gRPCServer.Services;
+
+public class DownloadFileService : DownloadFileStreaming.DownloadFileStreamingBase
+{
+    private readonly AppDbContext _appDbContext;
+    public DownloadFileService(AppDbContext appDbContext) => _appDbContext = appDbContext;
+    public async override Task DownloadFileById(FileRequestById request, IServerStreamWriter<FileContent> responseStream, ServerCallContext context)
+    {
+        var fileRecord = await _appDbContext.File.FirstOrDefaultAsync(f => f.Id == request.Id);
+
+        if (fileRecord == null)
+        {
+            throw new RpcException(new Status(StatusCode.NotFound, "File not found"));
+        }
+
+        var fileBytes = fileRecord.Content;
+        var chunkSize = 1024 * 64; 
+        var fileName = fileRecord.FileName; 
+
+        await responseStream.WriteAsync(new FileContent
+        {
+            Content = Google.Protobuf.ByteString.Empty, 
+            FileName = fileName,
+            ChunkSize = fileBytes.Length
+        });
+
+        for (int i = 0; i < fileBytes.Length; i += chunkSize)
+        {
+            var chunk = fileBytes.Skip(i).Take(chunkSize).ToArray();
+
+            await responseStream.WriteAsync(new FileContent
+            {
+                Content = Google.Protobuf.ByteString.CopyFrom(chunk),
+                ChunkSize = chunk.Length
+            });
+
+            Console.WriteLine($"Sent {Math.Min(i + chunkSize, fileBytes.Length)} of {fileBytes.Length} bytes...");
+        }
+    }
+}
