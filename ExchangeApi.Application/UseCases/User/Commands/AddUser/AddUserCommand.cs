5fe15dfe5cf2d1f5684332af1b29c3692de93d14
Farbod <mr.farbodmirzaee@gmail.com>
2025-05-26 09:21:10 +0330

    Implement password hashing with Argon2 in AddUserCommand; update DataSeeder for hashed password generation and add PasswordHelper class

--- ExchangeApi.Application/UseCases/User/Commands/AddUser/AddUserCommand.cs
+++ ExchangeApi.Application/UseCases/User/Commands/AddUser/AddUserCommand.cs
@@ -8,6 +8,8 @@ public record AddUserCommand : IRequest<Response<bool>>
 {
     public string Name { get; set; }
     public string UserName { get; set; }
+    public string MetaDescription { get; set; }
+    public string Description { get; set; }
     public string EmailAddress { get; set; }
     public string Password { get; set; }
     public bool IsActive { get; set; }
